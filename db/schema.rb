eeac62a2321e9550b7cf41a8d27c03706b2e3031
Jerry Hoglen <jerryhoglen@me.com>
2017-01-23 09:34:01 -0500

    styled blog site

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121024256) do
+ActiveRecord::Schema.define(version: 20170123021855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string   "name"
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
